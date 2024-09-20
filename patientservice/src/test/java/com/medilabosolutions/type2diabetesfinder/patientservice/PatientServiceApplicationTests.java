f3773b065ae8d152e7bf450ac9a4e50808a7783d
OlivierMOREL <oliviermorel.oc1@gmail.com>
2024-09-20 15:47:34 +0200

    feat : add patientService new SpringBoot project

---
+++ patientservice/src/test/java/com/medilabosolutions/type2diabetesfinder/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.type2diabetesfinder.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
