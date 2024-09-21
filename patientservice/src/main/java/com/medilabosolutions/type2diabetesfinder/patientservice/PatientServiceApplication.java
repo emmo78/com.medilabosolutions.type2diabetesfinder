15ef9f351d40235d55106201fc543a2c5c44c326
OlivierMOREL <oliviermorel.oc1@gmail.com>
2024-09-21 17:38:28 +0200

    feat : configure .properties

--- patientservice/src/main/java/com/medilabosolutions/type2diabetesfinder/patientservice/PatientServiceApplication.java
+++ patientservice/src/main/java/com/medilabosolutions/type2diabetesfinder/patientservice/PatientServiceApplication.java
@@ -2,8 +2,10 @@ package com.medilabosolutions.type2diabetesfinder.patientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource({"file:./patientservice/db.properties"})
 public class PatientServiceApplication {
 
 	public static void main(String[] args) {
