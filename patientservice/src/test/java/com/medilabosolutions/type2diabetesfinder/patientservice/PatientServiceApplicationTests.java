c0e82f32c9853620155f260ce9be4d7b6b45abbb
omorel <oliviermorel.oc1@gmail.com>
2024-09-24 16:19:17 +0200

    feat : patient validation, TU and IT done

--- patientservice/src/test/java/com/medilabosolutions/type2diabetesfinder/patientservice/PatientServiceApplicationTests.java
+++ patientservice/src/test/java/com/medilabosolutions/type2diabetesfinder/patientservice/PatientServiceApplicationTests.java
@@ -1,13 +1,18 @@
 package com.medilabosolutions.type2diabetesfinder.patientservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@Slf4j
 @SpringBootTest
+@ActiveProfiles("mytest")
 class PatientServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log.info("PatientServiceApplicationTests");
 	}
 
 }
