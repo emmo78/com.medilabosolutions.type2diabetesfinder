8f0c71540e262f348154a8f7088e3f26490ed2b9
OlivierMOREL <oliviermorel.oc1@gmail.com>
2024-10-06 19:14:59 +0200

    feat : ControllerExceptionHandler

---
+++ patientservice/src/main/java/com/medilabosolutions/type2diabetesfinder/patientservice/service/RequestServiceImpl.java
@@ -0,0 +1,28 @@
+package com.medilabosolutions.type2diabetesfinder.patientservice.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.WebRequest;
+
+@Service
+public class RequestServiceImpl implements RequestService{
+    @Override
+    public String requestToString(WebRequest request) {
+        //uri in StringBuffer
+        StringBuffer parameters = new StringBuffer(request.getDescription(false)+"?");
+        //p = parameter key of values v = String[]
+        request.getParameterMap().forEach((p,v) -> {
+            if (!p.equals("password")) {
+                parameters.append(p + "=");
+                int i = 0;
+                while (i < (v.length - 1)) {
+                    parameters.append(v[i] + ",");
+                    i++;
+                }
+                parameters.append(v[i] + "&");
+            }
+        });
+        int length = parameters.length();
+        parameters.delete(length-1, length);
+        return parameters.toString();
+    }
+}
