4479f24b8b7edcdfa1594e8d23baf9d7c151a198
omorel <omorel@ardans.fr>
2025-03-13 16:02:56 +0100

    wip : eurekaService 20250313

---
+++ eurekaservice/src/test/java/com/medilabosolutions/type2diabetesfinder/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabosolutions.type2diabetesfinder.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
